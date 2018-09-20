ee00ad19453ec761e41194785a46b91f16572b15
Guilhemar Caixeta filho <guilhemarcaixeta@gmail.com>
2018-09-19 21:28:09 -0700

    changed the header

--- libft.h
+++ libft.h
@@ -119,4 +119,7 @@ void			ft_mtx_print(t_mtx_lst *mlst);
 void			ft_print_tab(char **s);
 int				get_next_line(const int fd, char **line);
 
+t_list			*ft_lstnew2(char *content);
+void			ft_lstdel2(t_list **alst)
+
 #endif
